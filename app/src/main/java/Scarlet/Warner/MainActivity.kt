97f710872fddaad89513dbf428824e1ecec7012c
ScarletWarner <warner.scarlet@gmail.com>
2021-07-22 23:44:05 -0400

    main activity
    Main activity almost done

--- app/src/main/java/Scarlet/Warner/MainActivity.kt
+++ app/src/main/java/Scarlet/Warner/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,37 +13,70 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun go2Home (view: View){
+            var intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+        fun go2Ranges (view: View){
+            var intent=Intent(this,Ranges::class.java)
+            startActivity(intent)
+        }
+
+        fun go2Pref (view: View){
+            var intent=Intent(this,Preferences::class.java)
+            startActivity(intent)
+        }
         fun go2Help (view: View){
             var intent=Intent(this,Help::class.java)
             startActivity(intent)
         }
 
-        /*fun onCalculateButtonClicked (view:View) {
-
-            var height=enterHeight.number
-            var age=enterAge.number
-            var weight=enterWeight.number
-
-            var cals: arrayOf(0,0,0)
-            var protein: arrayOf(0,0,0)
-            var carbs: arrayOf(0,0,0)
-            var fat: arrayOf(0,0,0)
-
-            when {
-                genderGroup.female.isChecked {
-                    female()
+        fun getdata (view: View){
+            var gender: String?=null
+                when {
+                    groupGender.radioFemale.isChecked -> gender = "female"
+                    groupGender.radioMale.isChecked -> gender = "male"
                 }
-                    genderGroup.male.isChecked ->
+            var age=inputAge.text
+            var height=inputHeight.text
+            var weight=inputWeight.text
+
+            var activity: Double?=null
+                when {
+                    groupActivity.radioBMR.isChecked -> activity = 1.0
+                    groupActivity.radioSed.isChecked -> activity = 1.2
+                    groupActivity.radioMod.isChecked -> activity = 1.4
+                    groupActivity.radioActive.isChecked -> activity = 1.6
+                    groupActivity.radioVActive.isChecked -> activity = 1.8
+                    groupActivity.radioEActive.isChecked -> activity = 1.9
                 }
-            }
-            }
-        fun female{
 
+            var goal: Int?=null
+                when {
+                    groupGoal.radioELoss.isChecked -> goal = -1000
+                    groupGoal.radioLoss.isChecked -> goal = -500
+                    groupGoal.radioMildLoss.isChecked -> goal = -250
+                    groupGoal.radioMaintain.isChecked -> goal = 0
+                    groupGoal.radioMildGain.isChecked -> goal = 250
+                    groupGoal.radioGain.isChecked -> goal = 500
+                    groupGoal.radioEGain.isChecked -> goal = 1000
+                }
         }
-        fun male{
 
+        fun conversions (view: View){
+            var kg = weight/2.205
+            var cm = height*2.54
         }
-        }*/
+
+        fun stjeor {
+            var cals
+            if (gender= "female") {
+                cals = (10*kg) +(6.25*cm)-(5*age)-161+goal
+            }
+            if (gender= "male") {
+                cals = (10*kg) +(6.25*cm)-(5*age)+5+goal
+            }
+        }
 }
 }
 
--- app/src/main/java/Scarlet/Warner/Preferences.kt
+++ app/src/main/java/Scarlet/Warner/Preferences.kt
@@ -2,10 +2,31 @@ package Scarlet.Warner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.content.Intent
 
 class Preferences : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
-    }
+
+
+        fun go2Home (view: View){
+            var intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+        fun go2Ranges (view: View){
+            var intent=Intent(this,Ranges::class.java)
+            startActivity(intent)
+        }
+
+        fun go2Pref (view: View){
+            var intent=Intent(this,Preferences::class.java)
+            startActivity(intent)
+        }
+        fun go2Help (view: View){
+            var intent=Intent(this,Help::class.java)
+            startActivity(intent)
+        }}
 }
