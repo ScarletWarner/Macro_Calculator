0c024cdfd6389bffa83d90d8291107630dfa912a
ScarletWarner <warner.scarlet@gmail.com>
2021-07-28 23:32:15 -0400

    4b
    "Fully functioning" app, still have a view layout tweaks to make it pretty
    main activity sends data to secondary activity, preferences sends data to secondary activity

--- app/src/main/java/Scarlet/Warner/MainActivity.kt
+++ app/src/main/java/Scarlet/Warner/MainActivity.kt
@@ -6,95 +6,102 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import kotlinx.android.synthetic.main.activity_main.view.*
+import android.content.SharedPreferences
 
 
 
 class MainActivity : AppCompatActivity() {
+
+    var myPref: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-        fun go2Home (view: View){
-            var intent=Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }
-        fun go2Ranges (view: View){
-            var intent=Intent(this,Ranges::class.java)
-            startActivity(intent)
-        }
 
-        fun go2Pref (view: View){
-            var intent=Intent(this,Preferences::class.java)
-            startActivity(intent)
-        }
-        fun go2Help (view: View){
-            var intent=Intent(this,Help::class.java)
-            startActivity(intent)
-        }
-
-        //pull data from screen
-        fun getdata (view: View){
+        fun setdata(view: View) {
 
             //set gender
-            var gender: String?=null
-                when {
-                    groupGender.radioFemale.isChecked -> gender = "female"
-                    groupGender.radioMale.isChecked -> gender = "male"
-                }
+            var gender: String? = null
+            when {
+                groupGender.radioFemale.isChecked -> gender = "female"
+                groupGender.radioMale.isChecked -> gender = "male"
+            }
 
             //bio variables
-            var age=inputAge.text
-            var height=inputHeight.text
-            var weight=inputWeight.text
+            var age : Int=inputAge.text.toString().toInt()
+            var height : Int= inputHeight.text.toString().toInt()
+            var weight :Int = inputWeight.text.toString().toInt()
 
             //set activity
-            var activity: Double?=null
-                when {
-                    groupActivity.radioBMR.isChecked -> activity = 1.0
-                    groupActivity.radioSed.isChecked -> activity = 1.2
-                    groupActivity.radioMod.isChecked -> activity = 1.4
-                    groupActivity.radioActive.isChecked -> activity = 1.6
-                    groupActivity.radioVActive.isChecked -> activity = 1.8
-                    groupActivity.radioEActive.isChecked -> activity = 1.9
-                }
+            var activity: Double? = null
+            when {
+                groupActivity.radioBMR.isChecked -> activity = 1.0
+                groupActivity.radioSed.isChecked -> activity = 1.2
+                groupActivity.radioMod.isChecked -> activity = 1.4
+                groupActivity.radioActive.isChecked -> activity = 1.6
+                groupActivity.radioVActive.isChecked -> activity = 1.8
+                groupActivity.radioEActive.isChecked -> activity = 1.9
+            }
 
             //set goal
-            var goal: Int?=null
-                when {
-                    groupGoal.radioELoss.isChecked -> goal = -1000
-                    groupGoal.radioLoss.isChecked -> goal = -500
-                    groupGoal.radioMildLoss.isChecked -> goal = -250
-                    groupGoal.radioMaintain.isChecked -> goal = 0
-                    groupGoal.radioMildGain.isChecked -> goal = 250
-                    groupGoal.radioGain.isChecked -> goal = 500
-                    groupGoal.radioEGain.isChecked -> goal = 1000
-                }
-        }
-
-        //perform imperial to metric conversions
-        fun conversions (weight:Int, height:Int){
-            var kg :Double = weight/2.205
-            var cm :Double = height*2.54
-        }
-
-        //perfrom st jeor equation to find calories
-        fun stjeor(kg:Double,cm:Double,gender:String,age:Int,goal:Int) {
-            
-            var cals: Double=0.0
-            if (gender== "female") {
-                cals = (10*kg) +(6.25*cm)-(5*age)-161+goal
+            var goal: Int = 0
+            when {
+                groupGoal.radioELoss.isChecked -> goal = -1000
+                groupGoal.radioLoss.isChecked -> goal = -500
+                groupGoal.radioMildLoss.isChecked -> goal = -250
+                groupGoal.radioMaintain.isChecked -> goal = 0
+                groupGoal.radioMildGain.isChecked -> goal = 250
+                groupGoal.radioGain.isChecked -> goal = 500
+                groupGoal.radioEGain.isChecked -> goal = 1000
             }
-            if (gender== "male") {
-                cals = (10*kg) +(6.25*cm)-(5*age)+5+goal
-            }
-        }
 
-        //on button click send to ranges to display data
-        fun calculateRanges (view:View,cals:Double){                                                                                                                                                                                                                                                                                                                                                                               8
-            var intent=Intent(this,Ranges::class.java)
-            intent.putExtra("cals", cals)
+            //perform imperial to metric conversions
+            var kg: Double = (weight / 2.205)
+            var cm: Double = (height * 2.54)
+
+            //perfrom st jeor equation to find calories
+            var cals : Int = 0
+            if (gender == "female")
+               cals = Math.ceil((10 * kg) + (6.25 * cm) - (5 * age) - 161 + goal).toInt()
+
+            if (gender == "male")
+                cals = Math.ceil((10 * kg) + (6.25 * cm) - (5 * age) + 5 + goal).toInt()
+
+            myPref = getPreferences(MODE_PRIVATE)
+
+            var editor: SharedPreferences.Editor = (myPref as SharedPreferences).edit()
+
+            editor.putInt("cals", cals)
+            editor.commit()
+        }
+    }
+
+        fun send2Ranges(view: View) {
+            var intent = Intent(this, Ranges::class.java)
             startActivity(intent)
         }
-}
+
+        fun go2Home(view: View) {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        fun go2Ranges(view: View) {
+            var intent = Intent(this, Ranges::class.java)
+            startActivity(intent)
+        }
+
+        fun go2Pref(view: View) {
+            var intent = Intent(this, Preferences::class.java)
+            startActivity(intent)
+        }
+
+        fun go2Help(view: View) {
+            var intent = Intent(this, Help::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
 
 
--- app/src/main/java/Scarlet/Warner/Ranges.kt
+++ app/src/main/java/Scarlet/Warner/Ranges.kt
@@ -38,8 +38,7 @@ class Ranges : AppCompatActivity() {
         fun calculateRanges(view:View) {
 
             //ini variables
-            var cals = intent.getDoubleExtra("cals", 0.0)
-
+            var cals : Int=0
             var fat : Int=0
             var lowFat : Int=0
             var hiFat : Int=0
@@ -55,49 +54,52 @@ class Ranges : AppCompatActivity() {
             var databack: SharedPreferences? = null
             databack = getPreferences(0)
 
+            if (databack.contains("cals"))
+            databack.getInt("cals", cals)
+
             if (databack.contains("fatPref"))
-                fat = databack.getInt("fatPref")
+                databack.getInt("fatPref", fat)
             else fat = 2
 
             if (databack.contains("protPref"))
-                prot = databack.getInt("protPref")
+                databack.getInt("protPref", prot)
             else prot = 2
 
             if (databack.contains("carbPref"))
-                carb = databack.getInt("carbPref")
+                carb = databack.getInt("carbPref", carb)
             else carb = 2
 
             //set fat range
             when (fat) {
-                1-> lowFat = ((.15 * cals) / 9)
-                1-> hiFat = Math.ceil((.25 * cals) / 9)
+                1-> lowFat = Math.ceil((.15 * cals) / 9).toInt()
+                1-> hiFat = Math.ceil((.25 * cals) / 9).toInt()
 
-                2-> lowFat = Math.ceil((.25 * cals) / 9)
-                2-> hiFat = Math.ceil((.35 * cals) / 9)
+                2-> lowFat = Math.ceil((.25 * cals) / 9).toInt()
+                2-> hiFat = Math.ceil((.35 * cals) / 9).toInt()
 
-                3-> lowFat = Math.ceil((.30 * cals) / 9)
-                3-> hiFat = Math.ceil((.40 * cals) / 9)
+                3-> lowFat = Math.ceil((.30 * cals) / 9).toInt()
+                3-> hiFat = Math.ceil((.40 * cals) / 9).toInt()
                 }
 
 
             //set protein range
             when (prot) {
-                2-> defProt = Math.ceil((.25 * cals)/4)
-                3-> defProt = Math.ceil((.40 * cals)/4)
+                2-> defProt = Math.ceil((.25 * cals)/4).toInt()
+                3-> defProt = Math.ceil((.40 * cals)/4).toInt()
                 }
 
             when (prot) {
-                2-> hiProt = Math.ceil((.35 * cals)/4)
-                3-> hiProt = Math.ceil((.50 * cals)/4)
+                2-> hiProt = Math.ceil((.35 * cals)/4).toInt()
+                3-> hiProt = Math.ceil((.50 * cals)/4).toInt()
             }
             // set carb range
             when (carb) {
-                1-> lowCarb = Math.ceil((.1 * cals)/4)
-                1-> hiCarb = Math.ceil((.3 * cals)/4)
-                2-> lowCarb = Math.ceil((.3 * cals)/4)
-                2-> hiCarb = Math.ceil((.5 * cals)/4)
-                3-> lowCarb = Math.ceil((.4 * cals)/4)
-                3-> hiCarb = Math.ceil((.6 * cals)/4)
+                1-> lowCarb = Math.ceil((.1 * cals)/4).toInt()
+                1-> hiCarb = Math.ceil((.3 * cals)/4).toInt()
+                2-> lowCarb = Math.ceil((.3 * cals)/4).toInt()
+                2-> hiCarb = Math.ceil((.5 * cals)/4).toInt()
+                3-> lowCarb = Math.ceil((.4 * cals)/4).toInt()
+                3-> hiCarb = Math.ceil((.6 * cals)/4).toInt()
                 }
 
             // apply text results
